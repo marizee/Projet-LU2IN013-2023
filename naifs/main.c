2eb6eec4a4742b6a17d8b49e3ae8123e00c8530f
Marie Bonboire <96301497+marizee@users.noreply.github.com>
2023-03-02 13:53:49 +0100

    Update main.c

--- naifs/main.c
+++ naifs/main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include "polynome.h"
 
 int main() {
+    
     Polynome* p1 = initialiser_polynome();
     Polynome* p2 = initialiser_polynome();
-    affiche_polynome(p1);
-    affiche_polynome(p2);
-    affiche_polynome(addition(p1,p2));
-    affiche_polynome(multiplication(p1,p2));
+    Polynome* p3 = katsubara(p1,p2);
+    Polynome* p4 = mult_naive(p1,p2);
+    
+    printf("mult par katsubara : "); affiche_polynome(p3);
+    printf("mult naïve : "); affiche_polynome(p4);
+
+    detruire_polynome(p1);
+    detruire_polynome(p2);
+    detruire_polynome(p3);
+    detruire_polynome(p4);
+    
     return 0;
 }
