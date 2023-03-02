134d760a77ebfc5f3dffb4362679e6b56407f109
Marie Bonboire <96301497+marizee@users.noreply.github.com>
2023-03-02 13:49:36 +0100

    Update polynome.h

--- naifs/polynome.h
+++ naifs/polynome.h
@@ -1,23 +1,12 @@
-#ifndef POLYNOME_H
-#define POLYNOME_H
-typedef struct _poly{
-    int n;
-    char indet; 
-    int* coeff;
-} Polynome;
+#ifndef OPNAIVES_H
+#define OPNAIVES_H
 
-Polynome* creer_polynome(int degre, char indet);
-
-void detruire_polynome(Polynome* p);
-
-Polynome* initialiser_polynome();
+#include "structure.h"
+#include <math.h>
 
 Polynome* addition(Polynome* p1, Polynome* p2);
-
+Polynome* soustraction(Polynome* p1, Polynome* p2);
 Polynome* mult_naive(Polynome* p1, Polynome* p2);
-
-void affiche_polynome(Polynome* p);
-
-/*a completer au fur et a mesure qu'on avance*/
+Polynome* katsubara(Polynome* f, Polynome* g);
 
 #endif
