bb30211c3c804e7b14b397902fd5cda2d481dc9a
Marie Bonboire <96301497+marizee@users.noreply.github.com>
2023-03-02 13:48:28 +0100

    ajout soustraction() et algo de Katsubara

--- naifs/polynome.c
+++ naifs/polynome.c
@@ -3,44 +3,14 @@
 #include <string.h>
 #include "polynome.h"
 
-/*version naif*/
-
-Polynome* creer_polynome(int degre, char indet){
-    Polynome* p =(Polynome*)malloc(sizeof(Polynome));
-    p->n = degre+1;
-    p->coeff = (int*)malloc((degre+1)*sizeof(int));
-    for (int i=0; i<degre+1; i++) {
-        p->coeff[i] = 0;
-    }
-    p->indet = indet;
-    return p;
-}
-
-Polynome* initialiser_polynome(){
-    int a; 
-    char var;  
-    printf("Veuillez saisir \n- le degré de votre polynome:\t");
-    scanf(" %d",&a);
-    printf("- son indeterminée:\t");
-    scanf(" %c",&var);
-    Polynome* p = creer_polynome(a, var);
-    printf("- ses coefficients:");
-    for(int i=0; i<p->n; i++){
-        printf("\t");
-        scanf(" %d",&a);
-        p->coeff[i] = a;
-    }
-    return p;
-}
-
 Polynome* addition(Polynome* p1, Polynome* p2) {
     int degre_inf;
     Polynome* new_poly;
     if((p1->n)<=(p2->n)){
-        new_poly = creer_polynome(p2->n-1,"X");
+        new_poly = creer_polynome(p2->n-1);
         degre_inf = p1->n;
     }else{
-        new_poly = creer_polynome(p1->n-1,"X");
+        new_poly = creer_polynome(p1->n-1);
         degre_inf = p2->n;
     }
     for (int i=0; i<(new_poly->n); i++) {
@@ -56,9 +26,32 @@ Polynome* addition(Polynome* p1, Polynome* p2) {
     }
     return new_poly;
 }
+Polynome* soustraction(Polynome* p1, Polynome* p2) {
+    int degre_inf;
+    Polynome* new_poly;
+    if((p1->n)<=(p2->n)){
+        new_poly = creer_polynome(p2->n-1);
+        degre_inf = p1->n;
+    }else{
+        new_poly = creer_polynome(p1->n-1);
+        degre_inf = p2->n;
+    }
+    for (int i=0; i<(new_poly->n); i++) {
+        if(i<=degre_inf){
+            new_poly->coeff[i] = p1->coeff[i] - p2->coeff[i];
+        }else{
+            if(new_poly->n == p1->n){
+                new_poly->coeff[i] = p1->coeff[i];
+            }else{
+                new_poly->coeff[i] = p2->coeff[i];
+            }
+        }
+    }
+    return new_poly;
+}
 
 Polynome* mult_naive(Polynome* p1, Polynome* p2) {
-    Polynome* new_poly = creer_polynome((p1->n+p2->n)-2,'X');
+    Polynome* new_poly = creer_polynome((p1->n+p2->n)-2);
     for (int i=0; i<p1->n; i++) {
         for (int j=0; j<p2->n; j++) {
             new_poly->coeff[i+j] += p1->coeff[i]*p2->coeff[j];
@@ -67,23 +60,52 @@ Polynome* mult_naive(Polynome* p1, Polynome* p2) {
     return new_poly;
 }
 
-void affiche_polynome(Polynome* p) {
-    for (int i=0; i<p->n; i++) {
-        if (p->coeff[i] != 0) {
-            if (i==0) {printf("%d", p->coeff[i]);}
-            else if (i==1) {printf("%d%s", p->coeff[i], p->indet);}
-            else {printf("%d%s^%d", p->coeff[i], p->indet, i);}
-        }
-        if (i != p->n-1) {printf(" + ");}
-    }
-    printf("\n");
+Polynome* katsubara(Polynome* f, Polynome* g) {
     
+    //cas d'arrêt
+    if (f->n == 0) return f;
+    if (g->n == 0) return g;
+    if (f->n == 1 || g->n == 1) return mult_naive(f, g);
+    
+    int k = ceil(fmax(f->n, g->n)/2.);
+
+    //initialisation des 2 composantes de f et g
+    Polynome* f0 = creer_polynome(k-1);
+    Polynome* f1 = creer_polynome((f->n)-k-1);
+
+    Polynome* g0 = creer_polynome(k-1);
+    Polynome* g1 = creer_polynome((g->n)-k-1);
+
+    //affectation des coefficients
+    int i,j;
+    for (i=0; i < k; i++) f0->coeff[i] = f->coeff[i];
+    for (i=k, j=0; i < f->n; i++, j++) f1->coeff[j] = f->coeff[i]; 
+    int l,m;
+    for (l=0; l < k; l++) g0->coeff[l] = g->coeff[l];
+    for (l=k, m=0; l < g->n; l++, m++) g1->coeff[m] = g->coeff[l];
+    
+    //étapes de l'algorithme
+    Polynome* h1 = katsubara(f0, g0);
+    Polynome* h2 = katsubara(f1, g1);   
+    Polynome* h5 = katsubara(addition(f0, f1), addition(g0, g1));
+    Polynome* h7 = soustraction(soustraction(h5, h1), h2);
+    
+    //polynome X^k
+    Polynome* un = creer_polynome(k);
+    un->coeff[k] = 1;
+    //polynome X^{2k}
+    Polynome* deux = creer_polynome(2*k);
+    deux->coeff[2*k] = 1;
+
+    //reconstitution de h = fg
+    Polynome* h = addition(addition(h1, mult_naive(h7,un)), mult_naive(h2, deux));
+    
+    return h;
 }
 
-/*il y a d'autre fonctions a implementer biensur*/
 
 // TODO fonction pour "detruire" un polynome (clear, kill, ...) ok
 // TODO Z/pZ plutot que les entiers:
-//  --> arithmetique (+ , - , * , inversion) dans Z/pZ, dans un fichier separe
+//  --> arithmetique (+ , - , * , inversion) dans Z/pZ, dans un fichier separe ok
 //  --> dans ce fichier ici, realiser les operations sur les coefficients dans Z/pZ
 
