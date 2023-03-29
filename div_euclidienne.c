2169ea4392e660eb81b3be2508769e2f32097749
Fallou912003 <124890666+Fallou912003@users.noreply.github.com>
2023-03-29 03:37:07 +0200

    Update div_euclidienne.c

--- div_euclidienne.c
+++ div_euclidienne.c
@@ -1,23 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <math.h>
 #include "div_euclidienne.h"
 
-Polynome* division(Polynome* A,Polynome* B){
-    Polynome* R = creer_polynome(B->n-1);
-    Polynome* Q = creer_polynome(A->n-B->n);
-    Polynome* temp = creer_polynome(A->n-B->n);
-    int i=A->n;
-    int j=B->n;
-    while((R->n<B->n)&&(i>=0)&&((A->n-B->n)>=0)&&(j>=0)){
-        temp->coeff[A->n-B->n]=A->coeff[i]*inverse(B->coeff[j]);
-        Q->coeff[A->n-B->n]=A->coeff[i]*inverse(B->coeff[j]);
+//pas encore fini car elle renvoie une resultat fausse de calcul
+/*Polynome* division(Polynome* A,Polynome* B){
+    if((A->n)<(B->n)){
+        return A;
+    }
+    int k=A->n-B->n;
+    Polynome* R = creer_polynome(A->n-1);
+    Polynome* temp = creer_polynome(k);
+    int i=A->n-1;
+    int j=B->n-1;
+    int b=1;
+    while((i>=0)&&(j>=0)){
+        detruire_polynome(R);
+        temp->coeff[k]=A->coeff[i]*inverse_euclide(B->coeff[j]);
+        int z = A->n-B->n;
         A=soustraction(A, mult_naive(temp, B));
-        R=A;
-        temp->coeff[A->n-B->n]=0;
+        R = coeff_to_polynome(A->coeff,(A->n)-1);
+        k=A->n-B->n;
+        temp->coeff[z]=0;
         i--;
         j--;
+        
     }
-    detruire_polynome(Q);
     detruire_polynome(temp);
     return R;
-}
+}*/
