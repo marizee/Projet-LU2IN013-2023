96fbc4e1ddad1046d80a85c3c26850cb4ea0ff93
Fallou912003 <124890666+Fallou912003@users.noreply.github.com>
2023-03-31 03:18:52 +0200

    Update div_euclidienne.c

--- div_euclidienne.c
+++ div_euclidienne.c
@@ -4,8 +4,7 @@
 #include <math.h>
 #include "div_euclidienne.h"
 
-//pas encore fini car elle renvoie une resultat fausse de calcul
-/*Polynome* division(Polynome* A,Polynome* B){
+Polynome* division(Polynome* A,Polynome* B){
     if((A->n)<(B->n)){
         return A;
     }
@@ -15,18 +14,16 @@
     int i=A->n-1;
     int j=B->n-1;
     int b=1;
-    while((i>=0)&&(j>=0)){
-        detruire_polynome(R);
+     R = coeff_to_polynome(A->coeff,(A->n)-1);
+    while((i>=0)&&(j>=0)&&((R->n)>=(B->n))){
         temp->coeff[k]=A->coeff[i]*inverse_euclide(B->coeff[j]);
-        int z = A->n-B->n;
-        A=soustraction(A, mult_naive(temp, B));
-        R = coeff_to_polynome(A->coeff,(A->n)-1);
-        k=A->n-B->n;
-        temp->coeff[z]=0;
+        R=soustraction(R, mult_naive(temp, B));
+        temp->coeff[k]=0;
+        k--;
         i--;
         j--;
-        
     }
     detruire_polynome(temp);
+    ajuste_degre(R);
     return R;
-}*/
+}
