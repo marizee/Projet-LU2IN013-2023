f23de1b010f47474846290f8d8c47bd7418e4448
Fallou912003 <124890666+Fallou912003@users.noreply.github.com>
2023-05-23 04:23:45 +0200

    Update ntlcode.cpp correction de tous les bugs et tests et ajout d'Hornet

--- ntlcode.cpp
+++ ntlcode.cpp
@@ -1,8 +1,5 @@
 #include <NTL/ZZ.h>
-#include <NTL/mat_ZZ.h>
-#include <NTL/vec_ZZ.h>
 #include <NTL/ZZ_pX.h>
-#include <NTL/ZZ_pXFactoring.h>
 #include <NTL/tools.h>
 #include <iostream>
 #include <NTL/lzz_pX.h>
@@ -10,66 +7,89 @@
 using namespace NTL;
 using namespace std;
 
-ZZ_pX karatsuba(const ZZ_pX& f, const ZZ_pX& g) {
-    if (IsZero(f) || IsZero(g))
-        return ZZ_pX(0);
+void init_poly_with_coeffs(zz_pX& poly, const vec_zz_p& coeffs) {
+    long n = coeffs.length();
+    random(poly,n);
+    for (long i = 0; i < n; i++) {
+        SetCoeff(poly, i, coeffs[i]);
+    }
+}
 
+zz_pX karatsuba(const zz_pX& f, const zz_pX& g) {
+    if (IsZero(f) || IsZero(g))
+        return zz_pX(0);
     long df = deg(f);
     long dg = deg(g);
     if (df <= 10 && dg <= 10)
         return f * g;
-
     long k = max(df / 2, dg / 2);
 
-    ZZ_pX f0, f1;
-    SetLength(f0, k);
-    SetLength(f1, df - k + 1);
+    zz_pX f0, f1;
+    random(f0, k);
+    random(f1, df-k+1);
     for (long i = 0; i < k; i++)
         f0[i] = f[i];
     for (long i = k; i <= df; i++)
         f1[i - k] = f[i];
 
-    ZZ_pX g0, g1;
-    SetLength(g0, k);
-    SetLength(g1, dg - k + 1);
+    zz_pX g0, g1;
+    random(g0, k);
+    random(g1, dg-k+1);
     for (long i = 0; i < k; i++)
         g0[i] = g[i];
     for (long i = k; i <= dg; i++)
         g1[i - k] = g[i];
 
-    ZZ_pX h1 = karatsuba(f0, g0);
-    ZZ_pX h2 = karatsuba(f1, g1);
-    ZZ_pX h5 = karatsuba(f0 + f1, g0 + g1);
-    ZZ_pX h7 = h5 - h1 - h2;
+    zz_pX h1 = karatsuba(f0, g0);
+    zz_pX h2 = karatsuba(f1, g1);
+    zz_pX h5 = karatsuba(f0 + f1, g0 + g1);
+    zz_pX h7 = h5 - h1 - h2;
 
-    ZZ_pX h;
+    zz_pX h;
     LeftShift(h7, h7, k);
     LeftShift(h2, h2, 2 * k);
     h = h1 + h7 + h2;
 
+    f0.kill();
+    f1.kill();
+    g0.kill();
+    g1.kill();
+    h1.kill();
+    h2.kill();
+    h5.kill();
+    h7.kill();
+
     return h;
 }
 
-ZZ_pX eval_naive_improved(const ZZ_pX& g, const ZZ_pX& a, const ZZ_pX& f) {
-    ZZ_pX res = g[0];
-    ZZ_pX ai = a % f;
-    long n = g.degree();
+zz_pX eval_naive_improved(const zz_pX& g, const zz_pX& a, const zz_pX& f) {
+
+    long n = deg(g);
+    zz_pX res;
+    random(res,n+1);
+    SetCoeff(res, 0, g[0]);
+    zz_pX ai = a % f;
 
     for (long i = 1; i <= n; i++) {
         res += g[i] * ai;
         ai = (a * ai) % f;
     }
-
+    ai.kill();
     return res % f;
 }
 
-ZZ_pX brentkung(const ZZ_pX& g, const ZZ_pX& a, const ZZ_pX& f) {
-    ZZ_pXContext context;
-    context.save();
-    context.restore();
+zz_pX horner(const zz_pX& g, const zz_pX& a, const zz_pX& f) {
+    zz_pX res;
+    SetCoeff(res, 0, coeff(g, deg(g)));
+    
+    for (long i = deg(g) - 1; i >= 0; i--) {
+        res = (res*a)%f + coeff(g, i);
+    }
+    
+    return res;
+}
 
-    ZZ_pXModulus F;
-    F.SetMod(f);
+zz_pX brentkung(const zz_pX& g, const zz_pX& a, const zz_pX& f) {
 
     long d = deg(g) + 1;
     long n = deg(f);
@@ -80,23 +100,23 @@ ZZ_pX brentkung(const ZZ_pX& g, const ZZ_pX& a, const ZZ_pX& f) {
     long r = long(ceil(sqrt(d)));
     long s = long(ceil(d / double(r)));
 
-    vec_ZZ_pX ac;
+    vec_zz_pX ac;
     ac.SetLength(r + 1);
-    ac[0] = 1;
+    ac[0] = to_zz_pX(1);
 
     for (long i = 1; i <= r; i++) {
         ac[i] = (a * ac[i - 1]) % f;
     }
 
-    mat_ZZ_p ma;
+    mat_zz_p ma;
     ma.SetDims(r, n);
     for (long i = 0; i < r; i++) {
         for (long j = 0; j < n; j++) {
-            ma[i][j] = rep(ac[i])[j];
+            ma[i][j] = rep((ac[i])[j]);
         }
     }
 
-    mat_ZZ_p mg;
+    mat_zz_p mg;
     mg.SetDims(s, r);
     for (long i = 0; i < s; i++) {
         for (long j = 0; j < r; j++) {
@@ -104,38 +124,45 @@ ZZ_pX brentkung(const ZZ_pX& g, const ZZ_pX& a, const ZZ_pX& f) {
         }
     }
 
-    mat_ZZ_p mb;
+    mat_zz_p mb;
     mb.SetDims(s, n);
     mb = mg * ma;
 
-    vec_ZZ_p b;
+    vec_zz_pX b;
     b.SetLength(s);
     for (long i = 0; i < s; i++) {
-        b[i] = rep(mb[i]);
+          init_poly_with_coeffs(b[i],mb[i]);
     }
 
-    ZZ_pX res;
+    zz_pX res;
     res = b[0];
-    ZZ_pX ar = ac[r];
+    zz_pX ar = ac[r];
+    
+    zz_pX tmp = ar;
 
-    ZZ_pX tmp = ar;
     for (long i = 1; i < s; i++) {
         res += (b[i] * tmp) % f;
         tmp = ar * tmp;
     }
 
+    ar.kill();
+    tmp.kill();
+    mb.kill();
+    mg.kill();
+    ma.kill();
+
     return res;
 }
 
+
 int main () 
 {
-	  //zz_p::init(GenPrime_long(60));
-		zz_p::init(97);
+    zz_p::init(97);
 
-    // déclarer 2 polynômes dans Z/pZ
+    // Déclarer 2 polynômes dans Z/pZ
     zz_pX p1, p2;
 
-    // initialiser les polynômes avec des coefficients
+    // Initialiser les polynômes avec des coefficients
     SetCoeff(p1, 0, 1);
     SetCoeff(p1, 1, 2);
     SetCoeff(p1, 2, 3);
@@ -143,54 +170,68 @@ int main ()
     SetCoeff(p2, 1, 5);
     SetCoeff(p2, 2, 6);
 
-    // faire les opérations entre les deux polynômes
+    // Faire les opérations entre les deux polynômes
     zz_pX p3 = p1 + p2; 
     zz_pX p4 = p1 * p2; 
     zz_pX p5 = p1 / p2; 
 
-    // afficher les valeurs obtenues
-    std::cout << "p1 + p2 = " << p3 << std::endl;
-    std::cout << "p1 * p2 = " << p4 << std::endl;
-    std::cout << "p1 / p2 = " << p5 << std::endl;
+    // Afficher les valeurs obtenues
+    cout << "p1 + p2 = " << p3 << endl;
+    cout << "p1 * p2 = " << p4 << endl;
+    cout << "p1 / p2 = " << p5 << endl;
 
-		// mesurer le temps:
-		long d = 100000;
-		zz_pX m, n;
-		random(m, d);
-		random(n, d);
-		double tstart, tend;
-		tstart = GetWallTime();
-		zz_pX c = m*n;
-		tend = GetWallTime();
-		std::cout << "m * n = " << c << std::endl;
-		std::cout << "temps pour multiplication: " << tend - tstart << std::endl;
-	
-        	ZZ_pX g, a, f;
-        	random(g, d);
-		random(a, 1000);
-        	random(f, 10000);
-		
-		double tstartk, tendk;
-        	tstartk = GetWallTime();
-		ZZ_pX result_karatsuba = karatsuba(g, f);
-		tendk = GetWallTime();
-		std::cout << "karatsuba(g, f) = " << result_karatsuba << std::endl;
-		std::cout << "time karatsuba: " << tendk - tstartk << std::endl;
+    // Mesurer le temps :
+    long d = 100;
+    long e = 10;
+    zz_pX g, f, a;
+    random(g, d);
+    random(f, d);
+    random(a, e);
 
-	
-		double tstartn, tendn;
-        	tstartn = GetWallTime();
-		ZZ_pX result_naive = eval_naive_improved(g, a, f);
-		tendn = GetWallTime();
-		std::cout << "g(a) % f = " << result_naive << std::endl;
+    double tstart, tend;
+    tstart = GetTime();
+    zz_pX mult = g * f;
+    tend = GetTime();
+    cout << "m * n = " << mult << endl;
+    cout << "Temps pour multiplication : " << tend - tstart << endl;
+    
+    double tstartk, tendk;
+    tstartk = GetTime();
+    zz_pX result_karatsuba = karatsuba(g, f);
+    tendk = GetTime();
+    cout << "karatsuba(g, f) = " << result_karatsuba << endl;
+    cout << "Temps pour karatsuba : " << tendk - tstartk << endl;
+
+    double tstartn, tendn;
+    tstartn = GetTime();
+		zz_pX result_naive = eval_naive_improved(g, a, f);
+		tendn = GetTime();
+		std::cout << "g(a) mod f = " << result_naive << std::endl;
 		std::cout << "time algo naive: " << tendn - tstartn << std::endl;
 
-        	double tstartbk, tendbk;
-        	tstartbk = GetWallTime();
-		ZZ_pX result_brentkung = brentkung(g, a, f);
-		tendbk = GetWallTime();
-		std::cout << "g(a) % f = " << result_brentkung << std::endl;
+    double tstarth, tendh;
+    tstarth = GetTime();
+		zz_pX result_horner = horner(g, a, f);
+		tendh = GetTime();
+		std::cout << "g(a) mod f = " << result_horner << std::endl;
+		std::cout << "time horner: " << tendh - tstarth << std::endl;
+
+
+    double tstartbk, tendbk;
+    tstartbk = GetTime();
+		zz_pX result_brentkung = brentkung(g, a, f);
+		tendbk = GetTime();
+		std::cout << "g(a) mod f = " << result_brentkung << std::endl;
 		std::cout << "time brentkung: " << tendbk - tstartbk << std::endl;
+
+    
+    f.kill();
+    g.kill();
+    a.kill();
+    mult.kill();
+    result_karatsuba.kill();
+    result_brentkung.kill();
+    result_horner.kill();
 
     return 0;
 }
